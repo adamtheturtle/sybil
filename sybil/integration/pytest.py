89e43a17a464b2bd23f0201cbcabf120192ea03e
Adam Dangoor <adamdangoor@gmail.com>
2023-09-03 12:26:35 +0100

    Progress

--- sybil/integration/pytest.py
+++ sybil/integration/pytest.py
@@ -4,14 +4,14 @@ import os
 from inspect import getsourcefile
 from os.path import abspath
 from pathlib import Path
-from typing import Any, Callable, Dict, Iterator, Tuple, Optional, Union, TYPE_CHECKING
+from typing import Any, Callable, Dict, Iterator, Sequence, Tuple, Optional, Union, TYPE_CHECKING
 
 from _pytest._code.code import TerminalRepr, Traceback, ExceptionInfo
 from _pytest import fixtures
 from _pytest.fixtures import FuncFixtureInfo
 from _pytest._io import TerminalWriter
 from _pytest.main import Session
-from _pytest.nodes import Collector
+from _pytest.nodes import Collector, _NodeType
 from _pytest.python import Module
 import pytest
 
@@ -39,19 +39,21 @@ class SybilFailureRepr(TerminalRepr):
         for line in self.message.splitlines():
             tw.line(line)
         tw.line()
-        tw.write(self.item.parent.name, bold=True, red=True)
+        item_parent = self.item.parent
+        assert item_parent is not None
+        tw.write(item_parent.name, bold=True, red=True)
         tw.line(":%s: SybilFailure" % self.item.example.line)
 
 
 class SybilItem(pytest.Item):
 
-    def __init__(self, parent, sybil: 'Sybil', example: example.Example) -> None:
+    def __init__(self, parent: 'SybilFile', sybil: 'Sybil', example: example.Example) -> None:
         name = 'line:{},column:{}'.format(example.line, example.column)
         super(SybilItem, self).__init__(name, parent)
         self.example = example
         self.request_fixtures(sybil.fixtures)
 
-    def request_fixtures(self, names: tuple[str]) -> None:
+    def request_fixtures(self, names: Sequence[str]) -> None:
         # pytest fixtures dance:
         fm = self.session._fixturemanager
         closure = fm.getfixtureclosure(names, self)
@@ -67,11 +69,12 @@ class SybilItem(pytest.Item):
         )
         return self.example.path, self.example.line, info
 
-    def getparent(self, cls):
+    def getparent(self, cls: type[_NodeType]) -> Optional[_NodeType]:
         if cls is Module:
             return self.parent
         if cls is Session:
             return self.session
+        return None
 
     def setup(self) -> None:
         self._request._fillfixtures()
