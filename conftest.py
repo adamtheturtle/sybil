d86c12185989cf5b2581ac6b2a27da77bf43f0c4
Adam Dangoor <adamdangoor@gmail.com>
2023-10-11 11:15:12 +0100

    REVERT Test changes (unwanted)

--- conftest.py
+++ conftest.py
@@ -34,11 +34,11 @@ def _find_python_files() -> List[Tuple[Path, str]]:
 
 
 @pytest.fixture
-def all_python_files() -> List[Tuple[Path, str]]:
+def all_python_files():
     return _find_python_files()
 
 
-def pytest_generate_tests(metafunc) -> None:
+def pytest_generate_tests(metafunc):
     files = _find_python_files()
     ids = [str(f[0]) for f in files]
     if "python_file" in metafunc.fixturenames:
